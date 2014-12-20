c87ff1e9aa8da7a204201688c0ee60ed5bdcb65c
Kareem <kpjackson27@hotmail.com>
2014-12-20 17:28:36 -0500

    started setting up dashboard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,5 +28,31 @@ var app = angular.module('umxFrontendApp', [
       .state('landing', {
         url:'/landing',
         templateUrl: 'views/landing.html'
-     });
+     })
+      .state('dashboard', {
+        url:'/dashboard',
+        templateUrl: 'views/dashboard.html'
+      });
+
   });
+
+app.controller('Controller', function($scope, ModalService){
+
+  $scope.show = function(){
+    ModalService.showModal({
+      templateUrl: 'views/terms.html',
+      controller: 'ModalCtrl'
+    }).then(function(modal){
+      modal.element.modal();
+      modal.close.then(function(result){
+        $scope.message = 'test' + result;
+      });
+    });
+  };
+});
+
+app.controller('ModalCtrl', function($scope, close){
+  $scope.close = function(result){
+    close(result,500);
+  };
+});
