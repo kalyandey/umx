7322a01a1e63bd8207502f002268bbcc27b2108e
mhebert15 <mathew.hebert@gmail.com>
2015-02-19 15:14:45 -0500

    Updating dashboard

--- app/scripts/controllers/createCtrl.js
+++ app/scripts/controllers/createCtrl.js
@@ -1,10 +1,15 @@
 'use strict';
 
-app.controller('CreateController', function ($scope, $modal){
-      $scope.openModal = function (){
-      $modal.open({
-          templateUrl: 'CreateModal.html',
-          controller: 'ModalCtrl'
-      });
+app.controller('CreateController', function ($scope, $modal) {
+  $scope.openModal = function () {
+    $modal.open({
+      templateUrl: 'CreateModal.html',
+      controller: 'ModalCtrl'
+    });
+
+  };
+  $scope.linkItems = {
+    "Settings": "#",
+    "Sign Out": "/"
   };
 });
