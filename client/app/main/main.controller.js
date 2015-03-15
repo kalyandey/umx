5cad71926f27040b908b998d4ecca594d7ec5f99
Jose Gomez <k13gomez@gmail.com>
2015-03-15 11:10:24 -0400

    fixed nav

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,10 +2,4 @@
 
 angular.module('umxApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
   });
