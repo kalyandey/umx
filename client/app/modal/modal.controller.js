2f93ef55d2928148775b2e60d3e6e8af0fae7484
Jose Gomez <k13gomez@gmail.com>
2015-03-15 11:52:55 -0400

    moved ModalCtrl

--- client/app/modal/modal.controller.js
+++ client/app/modal/modal.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('umxApp')
-.controller('ModalCtrl', function ($scope, $modalInstance) {
+  .controller('ModalCtrl', function ($scope, $modalInstance) {
     $scope.cancel = function () {
       $modalInstance.dismiss('cancel');
     };
-});
+  });
